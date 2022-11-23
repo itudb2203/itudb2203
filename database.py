2613ab2c4818e79eac6e35d110f17b47c04fac75
bozkurta19 <58877974+bozkurta19@users.noreply.github.com>
2022-11-23 08:31:10 +0300

    Get number of pages dynamically from database and close cursors
    Added number of players method to database class. View function gets the number of players from this method and calculates number of pages and passes it to players.html. Also closed to cursors.

--- database.py
+++ database.py
@@ -15,4 +15,14 @@ class Database:
             cursor.execute(query, ((int(page_num) - 1) * 10,))
             for playerID, nameFirst, nameLast, birthYear, birthCountry, weight, height in cursor:
                 players_list.append(Player(playerID, nameFirst, nameLast, birthYear, birthCountry, weight, height))
+            cursor.close()
         return players_list
+
+    def get_num_players(self):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT COUNT (*) FROM Master AS num_players"
+            cursor.execute(query)
+            num_of_players = cursor.fetchone()[0]
+            cursor.close()
+            return num_of_players
