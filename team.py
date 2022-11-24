f16e7a5cd6af898fd7b4589d93acfa7608bdc784
ugurh20 <666altay@gmail.com>
2022-11-24 12:10:22 +0300

    Added the class template of the row to list

---
+++ team.py
@@ -0,0 +1,13 @@
+class Team:
+    def __init__(self, teamID,yearID, name, Rank, LgWin, G, W, L, R, E, park):
+        self.teamID = teamID
+        self.yearID = yearID
+        self.name = name
+        self.Rank = Rank
+        self.LgWin = LgWin
+        self.G = G
+        self.W = W
+        self.L = L
+        self.R = R
+        self.E = E
+        self.park = park
