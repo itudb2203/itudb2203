70ed935625ce9a747dba83e1af632cf2209b04c6
Arda Altıntaş <73823004+ardanyan@users.noreply.github.com>
2022-11-25 00:10:59 +0300

    Add files via upload

---
+++ batting.py
@@ -0,0 +1,8 @@
+class Batting:
+    def __init__(self, playerID, yearID, teamID, lgID, R, G):
+        self.playerID = playerID
+        self.yearID = yearID
+        self.teamID = teamID
+        self.lgID = lgID
+        self.R = R
+        self.G = G
