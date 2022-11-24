42bde3ce7d86527489e7e66ce8e1ff93f12337e4
ugurh20 <666altay@gmail.com>
2022-11-24 12:11:46 +0300

    Added functionality to display teams page

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3 as dbapi2
 from player import Player
+from team import Team
 
 class Database:
     def __init__(self, dbfile):
@@ -17,11 +18,30 @@ class Database:
                 players_list.append(Player(playerID, nameFirst, nameLast, birthYear, birthCountry, weight, height))
             cursor.close()
         return players_list
-
+    def get_teams(self, page_num):
+        teams_list = []
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = """SELECT teamID, yearID, name, Rank, LgWin, G, W, L, R, E, park FROM Teams
+            ORDER BY teamID,yearID
+            LIMIT 10 OFFSET ?"""
+            cursor.execute(query, ((int(page_num) - 1) * 10,))
+            for teamID, yearID, name, Rank, LgWin, G, W, L, R, E, park in cursor:
+                teams_list.append(Team(teamID, yearID, name, Rank, LgWin, G, W, L, R, E, park))
+            cursor.close()
+        return teams_list
+    def get_num_teams(self):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT COUNT (*) FROM Teams AS num_teams"
+            cursor.execute(query)
+            num_of_teams = cursor.fetchone()[0]
+            cursor.close()
+            return num_of_teams
     def get_num_players(self):
         with dbapi2.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "SELECT COUNT (*) FROM Master AS num_players"
+            query = "SELECT COUNT (*) FROM Teams AS num_players"
             cursor.execute(query)
             num_of_players = cursor.fetchone()[0]
             cursor.close()
--- views.py
+++ views.py
@@ -9,3 +9,8 @@ def players_page(page_num):
     players_list = myDB.get_players(page_num)
     num_of_pages = ceil(myDB.get_num_players() / 10)
     return render_template("players.html", players=players_list, cur_page = int(page_num), num_pages = num_of_pages)
+def teams_page(page_num):
+    myDB = current_app.config["dbconfig"]
+    teams_list = myDB.get_teams(page_num)
+    num_of_pages = ceil(myDB.get_num_teams() / 10)
+    return render_template("teams.html", teams=teams_list, cur_page = int(page_num), num_pages = num_of_pages)
