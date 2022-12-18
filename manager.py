539706732383086be8833e2fd362c79710b29a7a
ugurh20 <666altay@gmail.com>
2022-12-18 09:53:44 +0300

    created manager object file

---
+++ manager.py
@@ -0,0 +1,12 @@
+class Manager:
+    def __init__(self, playerID, yearID, teamID, lgID, inseason, G, W, L, rank, plyrMgr):
+        self.playerID = playerID
+        self.yearID = yearID
+        self.teamID = teamID
+        self.lgID = lgID
+        self.inseason = inseason
+        self.G = G
+        self.W = W
+        self.L = L
+        self.rank = rank
+        self.plyrMgr = plyrMgr
